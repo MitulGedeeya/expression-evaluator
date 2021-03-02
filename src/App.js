dbf2e568e00fd9638a170f51739d7ef2e58d9ba7
Mitul Gedeeya <imitul.karishyen@gmail.com>
2021-03-02 18:38:59 +0530

    React js test

--- src/App.js
+++ src/App.js
@@ -1,57 +1,133 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
 
-function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
+class App extends Component {
+    state = {
+        formulaParts: [],
+        firstOperand: null,
+        operand: null,
+        operator: '+',
+        answer: null,
+        formula: '',
+        showOperandError: false
     }
-    getDate();
-  }, []);
-  return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
-  );
+
+    setFirstOperand = () => {
+        if (this.state.firstOperand === null || this.state.firstOperand === undefined || this.state.firstOperand === '') {
+            this.setState({showOperandError: true})
+            return false
+        }
+
+        this.setState({
+            formulaParts: [this.state.firstOperand],
+            formula: `${this.state.firstOperand}`,
+            answer: this.state.firstOperand,
+            showOperandError: false
+        })
+    }
+
+
+    prepareAnswer = () => {
+        if (!this.state.operand || !this.state.operator) {
+            this.setState({showOperandError: true})
+            return false
+        }
+
+        const formula = `${this.state.formula} ${this.state.operator} ${this.state.operand}`
+        const answer = eval(formula)
+
+        this.setState({
+            formula,
+            answer,
+            formulaParts: [...this.state.formulaParts, this.state.operand, this.state.operator],
+            operand: '',
+            showOperandError: false
+        })
+
+        this.myFormRef.value = null
+    }
+
+    render() {
+
+        if (this.state.formulaParts.length === 0) {
+            return (
+                <div className="d-flex flex-wrap align-items-center justify-content-center screen">
+                    <div className="d-flex flex-wrap align-items-center justify-content-center">
+                        <div>
+                            <input
+                                type="number"
+                                className="form-control form-control-lg d-sm-w-50"
+                                placeholder="Please enter a number"
+                                value={this.state.firstOperand}
+                                onChange={(e) => {
+                                    this.setState({firstOperand: e.target.value})
+                                }}/>
+                            <div className="text-danger ml-2 text-left"
+                                 style={{visibility: `${this.state.showOperandError ? 'visible' : 'hidden' }`}}>Please enter a valid value
+                            </div>
+                        </div>
+                        <div>
+                            <button type="button" className="btn btn-lg mb-4 btn-outline-primary ml-4"
+                                    onClick={this.setFirstOperand}>Add
+                                Number
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+            )
+        }
+
+        return (
+            <div className="d-flex flex-wrap align-items-center justify-content-center screen">
+                <div className="flex-wrap align-items-center justify-content-center text-center evaluation-container">
+                    <div>
+                        <div className="d-flex flex-wrap justify-content-center">
+                            {
+                                this.state.formulaParts.map((operationItem, index) => {
+                                    return (
+                                        <div className="p-4 m-1 formula-part" key={index}>{operationItem}</div>
+                                    )
+                                })
+
+                            }
+                        </div>
+                        <div className="equals-text">=</div>
+                        <div className="mb-5 answer-text">{ this.state.answer.toString() }</div>
+                    </div>
+                    <div>
+                        <div className="d-flex">
+                            <div className="w-50 mx-2">
+                                <select className="custom-select custom-select-lg "
+                                        placeholder="Operator"
+                                        onChange={(e) => {
+                                            this.setState({operator: e.target.value})
+                                        }}>
+                                    <option value="+">+</option>
+                                    <option value="-">-</option>
+                                    <option value="*">*</option>
+                                    <option value="/">/</option>
+                                </select>
+                            </div>
+                            <div className="w-50 mx-2">
+                                <input type="number" className="form-control form-control-lg "
+                                       ref={(el) => this.myFormRef = el} placeholder="Operand"
+                                       onChange={(e) => {
+                                           this.setState({operand: e.target.value})
+                                       }}/>
+                                <div className="text-danger ml-2 text-left mt-2"
+                                     style={{visibility: `${this.state.showOperandError ? 'visible' : 'hidden' }`}}>Invalid operand
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <button onClick={this.prepareAnswer} className="btn btn-lg btn-outline-primary mt-5">Add
+                        Operation
+                    </button>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
